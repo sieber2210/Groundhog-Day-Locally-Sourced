d3dc1529a8567b37b7cafea9325c91b0cbbd4fc7
sieber2210 <jsieber53@gmail.com>
2021-01-15 02:45:03 -0500

    Added radio
    Found/added radio model
    Added animations for the radio to break
    Created a script to control the radio
    Current;y have a test button to break radio using the B key on keyboard

---
+++ Assets/Scripts/Misc/Radio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Radio : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void BreakRadio()
+    {
+        anim.SetTrigger("Break");
+    }
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B)) BreakRadio();
+    }
+#endif
+}
