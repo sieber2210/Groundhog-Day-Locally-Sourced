9e267f832309612524428dc1ba97a830d288b3dd
BlackHavokk <65984450+BlackHavokk@users.noreply.github.com>
2021-01-30 14:18:20 -0500

    FMOD

---
+++ Assets/AttackSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSound : MonoBehaviour
+{
+    float currAnim;
+    public Animator enemyAnimator;
+    public FMOD.Studio.EventInstance attackSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currAnim = enemyAnimator.GetCurrentAnimatorStateInfo(0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack1"))
+        {
+            attackSound.start();
+        }
+    }
+}
